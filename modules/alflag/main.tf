eff6eda6902a4f21beebbd442425f7f84c94e320
snake <admin@viasnake.com>
2024-01-29 01:52:55 +0900

    feat: dkim

--- modules/alflag/main.tf
+++ modules/alflag/main.tf
@@ -26,3 +26,10 @@ resource "cloudflare_record" "txt_dmarc" {
   value   = "v=DMARC1; p=none; rua=mailto:${var.email}"
   type    = "TXT"
 }
+
+resource "cloudflare_record" "txt_dkim" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "api._domainkey"
+  value   = "k=rsa;t=s;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbmGbQMzYeMvxwtNQoXN0waGYaciuKx8mtMh5czguT4EZlJXuCt6V+l56mmt3t68FEX5JJ0q4ijG71BGoFRkl87uJi7LrQt1ZZmZCvrEII0YO4mp8sDLXC8g1aUAoi8TJgxq2MJqCaMyj5kAm3Fdy2tzftPCV/lbdiJqmBnWKjtwIDAQAB"
+  type    = "TXT"
+}
