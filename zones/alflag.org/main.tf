b35e466f1bb097ed6a3951407f58f141db8aab2b
snake <admin@viasnake.com>
2024-03-13 00:58:11 +0900

    feat: YouTrack 関連のレコードを削除

--- zones/alflag.org/main.tf
+++ zones/alflag.org/main.tf
@@ -2,16 +2,6 @@ data "cloudflare_zone" "main" {
   name = var.zone
 }
 
-resource "cloudflare_record" "issues" {
-  zone_id         = data.cloudflare_zone.main.id
-  name            = "issues"
-  value           = "alflag.youtrack.cloud"
-  type            = "CNAME"
-  ttl             = "3600"
-  proxied         = false
-  allow_overwrite = true
-}
-
 resource "cloudflare_record" "txt_spf" {
   zone_id         = data.cloudflare_zone.main.id
   name            = "@"
