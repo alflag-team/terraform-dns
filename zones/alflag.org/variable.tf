1eee2773531898b3776b8e94a2a39afa611ee6f9
snake <admin@viasnake.com>
2024-03-15 01:03:48 +0900

    feat: DMARC レポートを配信しない

--- zones/alflag.org/variable.tf
+++ zones/alflag.org/variable.tf
@@ -3,9 +3,3 @@ variable "zone" {
   type        = string
   default     = "alflag.org"
 }
-
-variable "email" {
-  description = "DMARC email"
-  type        = string
-  default     = "admin@alflag.org"
-}
