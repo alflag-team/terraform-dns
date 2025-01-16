d03784a2e649f0f26d82bffbca5b255d2d20cf3f
snake <admin@viasnake.com>
2025-01-17 06:14:00 +0900

    update to latest records

--- zones/alflag.org/main.tf
+++ zones/alflag.org/main.tf
@@ -2,10 +2,17 @@ data "cloudflare_zone" "main" {
   name = var.zone
 }
 
+resource "cloudflare_record" "txt_google" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "@"
+  content = "google-site-verification=9LYYBMFLbQVP7G7N-wd5CoJnrbQm0MeYeMTn1ItoPdg"
+  type    = "TXT"
+}
+
 resource "cloudflare_record" "txt_spf" {
   zone_id         = data.cloudflare_zone.main.id
   name            = "@"
-  content         = "v=spf1 include:_spf.elasticemail.com include:_spf.mx.cloudflare.net ~all"
+  content         = "v=spf1 include:_spf.mx.cloudflare.net include:ap.rp.oracleemaildelivery.com ~all"
   type            = "TXT"
   allow_overwrite = true
 }
@@ -14,12 +21,5 @@ resource "cloudflare_record" "txt_dmarc" {
   zone_id = data.cloudflare_zone.main.id
   name    = "_dmarc"
   content = "v=DMARC1; p=quarantine; rua=mailto:14e67501080447ceb90a47e04b59eab1@dmarc-reports.cloudflare.net"
-  type    = "TXT"
-}
-
-resource "cloudflare_record" "txt_dkim" {
-  zone_id = data.cloudflare_zone.main.id
-  name    = "api._domainkey"
-  content = "k=rsa;t=s;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbmGbQMzYeMvxwtNQoXN0waGYaciuKx8mtMh5czguT4EZlJXuCt6V+l56mmt3t68FEX5JJ0q4ijG71BGoFRkl87uJi7LrQt1ZZmZCvrEII0YO4mp8sDLXC8g1aUAoi8TJgxq2MJqCaMyj5kAm3Fdy2tzftPCV/lbdiJqmBnWKjtwIDAQAB"
   type    = "TXT"
 }
--- zones/mcplay.biz/main.tf
+++ zones/mcplay.biz/main.tf
@@ -2,6 +2,15 @@ data "cloudflare_zone" "main" {
   name = var.zone
 }
 
+resource "cloudflare_record" "alflag-prod-20240818" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "alflag-prod-20240818._domainkey"
+  content = "alflag-prod-20240818.mcplay.biz.dkim.kix1.oracleemaildelivery.com"
+  type    = "CNAME"
+  ttl     = "60"
+  proxied = false
+}
+
 resource "cloudflare_record" "tcpshield" {
   zone_id = data.cloudflare_zone.main.id
   name    = "tcpshield"
@@ -11,10 +20,17 @@ resource "cloudflare_record" "tcpshield" {
   proxied = false
 }
 
+resource "cloudflare_record" "txt_google" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "@"
+  content = "google-site-verification=_9mfE9SsuSmc8huJAl3glGlMJn_VkcOCgH9ljQmTzUU"
+  type    = "TXT"
+}
+
 resource "cloudflare_record" "txt_spf" {
   zone_id = data.cloudflare_zone.main.id
   name    = "@"
-  content = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  content = "v=spf1 include:_spf.mx.cloudflare.net include:ap.rp.oracleemaildelivery.com ~all"
   type    = "TXT"
 }
 
