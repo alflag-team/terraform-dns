c98c8f94b79b756fcdafb8411bee06cf3497b322
snake <admin@viasnake.com>
2025-03-22 19:07:25 +0900

    update

--- zones/alflag.org/main.tf
+++ zones/alflag.org/main.tf
@@ -2,24 +2,44 @@ data "cloudflare_zone" "main" {
   name = var.zone
 }
 
-resource "cloudflare_record" "txt_google" {
+resource "cloudflare_record" "mx" {
+  for_each = {
+    aspmx0 = "aspmx.l.google.com"
+    aspmx1 = "alt1.aspmx.l.google.com"
+    aspmx2 = "alt2.aspmx.l.google.com"
+    aspmx3 = "alt3.aspmx.l.google.com"
+    aspmx4 = "alt4.aspmx.l.google.com"
+  }
   zone_id = data.cloudflare_zone.main.id
   name    = "@"
-  content = "google-site-verification=9LYYBMFLbQVP7G7N-wd5CoJnrbQm0MeYeMTn1ItoPdg"
-  type    = "TXT"
+  content = each.value
+  type    = "MX"
 }
 
-resource "cloudflare_record" "txt_spf" {
-  zone_id         = data.cloudflare_zone.main.id
-  name            = "@"
-  content         = "v=spf1 include:_spf.mx.cloudflare.net include:ap.rp.oracleemaildelivery.com ~all"
-  type            = "TXT"
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "txt_dmarc" {
+resource "cloudflare_record" "txt" {
+  for_each = {
+    spf = {
+      name    = "@"
+      content = "v=spf1 include:ap.rp.oracleemaildelivery.com include:_spf.google.com ~all"
+    }
+    dmarc = {
+      name    = "_dmarc"
+      content = "v=DMARC1; p=quarantine; rua=mailto:14e67501080447ceb90a47e04b59eab1@dmarc-reports.cloudflare.net"
+    }
+  }
   zone_id = data.cloudflare_zone.main.id
-  name    = "_dmarc"
-  content = "v=DMARC1; p=quarantine; rua=mailto:14e67501080447ceb90a47e04b59eab1@dmarc-reports.cloudflare.net"
+  name    = each.value.name
+  content = each.value.content
   type    = "TXT"
+}
+
+resource "cloudflare_record" "cname" {
+  for_each = {
+    name    = "alflag-prod-20240818._domainkey"
+    content = "alflag-prod-20240818.alflag.org.dkim.kix1.oracleemaildelivery.com"
+  }
+  zone_id = data.cloudflare_zone.main.id
+  name    = each.value.name
+  content = each.value.content
+  type    = "CNAME"
 }
