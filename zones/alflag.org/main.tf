f7b2440c4900832e5a5c06f8f138deb252aba15e
snake <admin@viasnake.com>
2025-04-28 02:00:35 +0900

    set ttl

--- zones/alflag.org/main.tf
+++ zones/alflag.org/main.tf
@@ -25,11 +25,13 @@ resource "cloudflare_record" "mx" {
       priority = 10
     }
   }
+
   zone_id  = data.cloudflare_zone.main.id
   name     = "@"
   content  = each.value.content
   priority = each.value.priority
   type     = "MX"
+  ttl      = "60"
 }
 
 resource "cloudflare_record" "txt" {
@@ -47,10 +49,12 @@ resource "cloudflare_record" "txt" {
       content = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78km4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB"
     }
   }
+
   zone_id = data.cloudflare_zone.main.id
   name    = each.value.name
   content = each.value.content
   type    = "TXT"
+  ttl     = "60"
 }
 
 resource "cloudflare_record" "cname_no_proxy" {
@@ -60,6 +64,7 @@ resource "cloudflare_record" "cname_no_proxy" {
       content = "alflag-prod-20240818.alflag.org.dkim.kix1.oracleemaildelivery.com"
     }
   }
+
   zone_id = data.cloudflare_zone.main.id
   name    = each.value.name
   content = each.value.content
